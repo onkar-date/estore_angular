27b32e32479a7de4a7874baeacb52ebd23f8c78c
onkar-date <onkar101197@gmail.com>
2024-10-12 22:47:43 +0530

    Home layout added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, HomeModule],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,10 +3,21 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
+const MATERIAL_MODULES_LIST = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatFormFieldModule,
+  MatSelectModule,
+];
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
-  exports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, ...MATERIAL_MODULES_LIST],
+  exports: [MatToolbarModule, ...MATERIAL_MODULES_LIST],
 })
 export class MaterialModule {}
