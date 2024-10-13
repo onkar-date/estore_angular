141a083edef9f483593672476c7ed3f0facc5b4d
onkar-date <onkar101197@gmail.com>
2024-10-13 18:50:22 +0530

    Added few navbar options

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  constructor(private router: Router) {}
 
+  gotToHome(): void {
+    this.router.navigate(['/home/all-products']);
+  }
+
+  goToAddProduct(): void {
+    this.router.navigate(['/home/add-product']);
+  }
 }
